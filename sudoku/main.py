56fe1e5758044c136005544200978ba6dc5f2c20
jreinval <jaakko.reinvall@aalto.fi>
2017-09-26 18:29:32 +0300

    added some todo-items

--- sudoku/main.py
+++ sudoku/main.py
@@ -1,6 +1,15 @@
 '''
+PREPARATIONS:
+* Python3
+* Sudoku board for numbers 1-4
+
 TODO:
-1. a
-2. b
-3. c
+* generate solvable sudoku board [jaakko] 
+
+board class: input: move/action, output: new board [eric]
+* verify input number that it fits the rules (group, horizontal, vertical) 
+* If game ends up in situation where no number can be put in without violating the rules return negative reward
+* If game ends up in fully filled board, return positive reward
+* make inputs from command line
+* board design (just aesthetics) [jaakko]
 '''
